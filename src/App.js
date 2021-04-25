0540ae53a850e2a85b50e2c67e92672d884df10d
smarqueslopez <smarqueslopez@gmail.com>
2021-04-25 20:14:19 +0200

    Adding router functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route exact path='/'></Route>
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
