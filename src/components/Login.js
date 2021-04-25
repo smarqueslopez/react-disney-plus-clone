945f8c7c711fd8522dee4aa49a616f5aa0cbbcfe
smarqueslopez <smarqueslopez@gmail.com>
2021-04-25 21:33:06 +0200

    Adding CTA in login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,9 @@ const Login = (props) => {
   return (
     <Container>
       <Content>
+        <CTA>
+          <CTALogoOne src='/images/cta-logo-one.svg' alt='cta-logo-one' />
+        </CTA>
         <BgImage />
       </Content>
     </Container>
@@ -39,9 +42,34 @@ const BgImage = styled.div`
   left: 0;
   bottom: 0;
   z-index: -1;
-  background-image: url("/images/login-background.jpg");
+  background-image: url('/images/login-background.jpg');
   background-size: cover;
   background-repeat: no-repeat;
+`
+
+const CTA = styled.div`
+  width: 100%;
+  max-width: 650px;
+  margin-top: 0;
+  margin-right: auto;
+  margin-bottom: 2vw;
+  margin-left: auto;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  transition-timing-function: ease-out;
+  transition: opacity 0.2s;
+`
+
+const CTALogoOne = styled.img`
+  width: 100%;
+  max-width: 600px;
+  min-height: 1px;
+  margin-bottom: 12px;
+  display: block;
 `
 
 export default Login
