a7b1e74ebba0e59f27e2909603c87cc60df56e37
smarqueslopez <smarqueslopez@gmail.com>
2021-04-27 23:09:13 +0200

    Storing movies from db firebase

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,61 @@ import Originals from './Originals'
 import Recommends from './Recommends'
 import Trending from './Trending'
 import Viewers from './Viewers'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import db from '../firebase'
+import { setMovies } from '../features/movie/movieSlice'
+import { selectUserName } from '../features/user/userSlice'
+import { useCallback } from 'react'
 
 const Home = (props) => {
+  const dispatch = useDispatch()
+  const userName = useSelector(selectUserName)
+
+  const setAllMovies = useCallback(
+    (movies) => {
+      dispatch(
+        setMovies({
+          recommend: movies.recommend,
+          newDisney: movies.newDisney,
+          original: movies.original,
+          trending: movies.trending
+        })
+      )
+    },
+    [dispatch]
+  )
+
+  useEffect(() => {
+    const movies = {
+      recommend: [],
+      newDisney: [],
+      original: [],
+      trending: []
+    }
+    db.collection('movies').onSnapshot((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        switch (doc.data().type) {
+          case 'recommend':
+            movies.recommend.push({ id: doc.id, ...doc.data() })
+            break
+          case 'new':
+            movies.newDisney.push({ id: doc.id, ...doc.data() })
+            break
+          case 'original':
+            movies.original.push({ id: doc.id, ...doc.data() })
+            break
+          case 'trending':
+            movies.trending.push({ id: doc.id, ...doc.data() })
+            break
+          default:
+            break
+        }
+      })
+      setAllMovies(movies)
+    })
+  }, [userName, setAllMovies])
+
   return (
     <Container>
       <ImgSlider />
