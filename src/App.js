0f008f087cfb1845f07ab93c0b85b323f142a029
smarqueslopez <smarqueslopez@gmail.com>
2021-04-27 23:46:43 +0200

    Adding Detail page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from './components/Login'
 import './App.css'
 import Header from './components/Header'
 import Home from './components/Home'
+import Detail from './components/Detail'
 
 function App() {
   return (
@@ -13,8 +14,11 @@ function App() {
           <Route exact path='/'>
             <Login />
           </Route>
-          <Route exact path='/home'>
+          <Route path='/home'>
             <Home />
+          </Route>
+          <Route path='/detail/:id'>
+            <Detail />
           </Route>
         </Switch>
       </Router>
