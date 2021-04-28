7560a1d479c8b7c90c4ecc0631c0b5467c4376c8
smarqueslopez <smarqueslopez@gmail.com>
2021-04-28 15:51:48 +0200

    Deleting useless css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Login from './pages/Login'
-import './App.css'
 import Header from './components/Header'
 import Home from './pages/Home'
 import Detail from './pages/Detail'
